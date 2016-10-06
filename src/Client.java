c9a943afb29e1c106406514288da9d54bb9d3677
pedrocfrazao <pedrocfrazao@gmail.com>
2016-10-06 20:20:06 -0300

    menu created

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.UUID;
+import java.util.Scanner;
 
 /**
  * Created by leonardo on 05/10/16.
@@ -11,22 +12,52 @@ public class Client {
 
     private static UUID clientId = UUID.randomUUID();
 
+    public static int menu() {
+
+        int option_teste;
+
+        Scanner input_exit = new Scanner(System.in);
+        System.out.println("----------------------------");
+        System.out.println("Options:");
+        System.out.println("[1] - Request resource"); //adicionar condicional para nao mostrar quando cliente ativo
+        System.out.println("[2] - Release resource");
+        System.out.println("[3] - Show waiting queue");
+        System.out.println("[0] - EXIT");
+        System.out.print("Enter option: ");
+        option_teste = input_exit.nextInt();
+
+        return option_teste;
+    }
+
+
     public static void main(String[] args) {
 
         String host = (args.length < 1) ? null : args[0];
+
+        menu();
 
         try {
             Registry registry = LocateRegistry.getRegistry(host);
             CoordinatorInterface stub = (CoordinatorInterface) registry.lookup("CoordinatorInterface");
 
-            stub.requestResource(clientId);
+            if (menu()==1) {
+                stub.requestResource(clientId);
 //            stub.releaseResource();
 //            stub.showRequestQueue();
+            }else if (menu()==2){
+                stub.releaseResource();
+            }else if (menu()==3){
+                stub.showRequestQueue();
+            }else{
+                //menu();
+                //sair
+            }
 
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (NotBoundException e) {
             e.printStackTrace();
         }
+
     }
 }
